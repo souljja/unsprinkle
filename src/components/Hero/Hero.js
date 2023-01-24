139c251a95c2ff0453a681cb3470ea64ca728f7a
Andrey_Bogoroditskiy <Andrey_Bogoroditskiy@epam.com>
2023-01-24 11:03:36 +0300

    - fix fonts size - fix images - fix a11y -fix tags

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,26 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+        <picture>
+            <source
+                type="image/avif"
+                srcSet="
+                    /images/hero-img.avif 1x,
+                    /images/hero-img@2x.avif 2x,
+                    /images/hero-img@3x.avif 3x
+                "
+            />
+            <source
+                type="image/jpg"
+                srcSet="
+                    /images/hero-img.jpg 1x,
+                    /images/hero-img@2x.jpg 2x,
+                    /images/hero-img@3x.jpg 3x
+                "
+            />
+            <HeroImage alt="" src="/images/hero-img.jpg" />
+        </picture>
+      <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
   );
 };
@@ -25,6 +43,8 @@ const HeroImage = styled.img`
   width: 500px;
   height: 500px;
   max-height: 100%;
+  object-fit: cover;
+  aspect-ratio: 1 / 1;
 `;
 
 const Swoop = styled.img`
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-const PhotoGridItem = ({ id, src, alt, tags }) => {
-  return (
-    <article>
-      <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
-      </Anchor>
-      <Tags>
-        {tags.map((tag) => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
-      </Tags>
-    </article>
-  );
+const PhotoGridItem = ({id, src, alt, tags}) => {
+    const imageName = src.replace(".jpg", "");
+    return (
+        <article>
+            <Anchor href={`/photos/${id}`}>
+                <picture>
+                    <source
+                        type="image/avif"
+                        srcSet={`
+                            ${imageName}.avif 1x,
+                            ${imageName}@2x.avif 2x,
+                            ${imageName}@3x.avif 3x
+                        `}
+                    />
+                    <source
+                        type="image/jpg"
+                        srcSet={`
+                            ${imageName}.jpg 1x,
+                            ${imageName}@2x.jpg 2x,
+                            ${imageName}@3x.jpg 3x
+                        `}
+                    />
+                    <Image alt={alt} src={src}/>
+                </picture>
+            </Anchor>
+            <Tags>
+                {tags.map((tag) => (
+                    <Tag key={tag}>{tag}</Tag>
+                ))}
+            </Tags>
+        </article>
+    );
 };
 
 const Anchor = styled.a`
@@ -26,22 +45,31 @@ const Image = styled.img`
   display: block;
   width: 100%;
   height: 300px;
+  object-fit: cover;
   border-radius: 2px;
   margin-bottom: 8px;
+  aspect-ratio: 1 / 1;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  margin-left: 8px;
+
+  &:first-of-type {
+    margin-left: 0;
+  }
 `;
 
 export default PhotoGridItem;
